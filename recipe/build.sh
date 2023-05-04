0e7086fa4da5200dea4b4b038e16a3bbee2b0e9f
Marek Waszkiewicz <mwaszkiewicz@anaconda.com>
2023-05-04 13:46:10 +0200

    update to 0.40.0

--- recipe/build.sh
+++ recipe/build.sh
@@ -8,4 +8,4 @@ export LLVMLITE_USE_CMAKE=1
 export LLVMLITE_SHARED=1
 
 $PYTHON setup.py build --force
-$PYTHON setup.py install
+$PYTHON -m pip install . --no-deps  --no-build-isolation --ignore-installed -vvv
