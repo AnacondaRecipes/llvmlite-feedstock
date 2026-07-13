4cf89f0fc213902e6083e8cd2c47a3752af0970a
Andrii Osipov <aosipov@anaconda.com>
2026-07-13 02:18:43 -0700

    switch llvm from 20 to 22

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,10 +2,14 @@
 
 set -x
 
+# CMake Error at CMakeLists.txt:46 (message):
+# LLVM CMake export states LLVM version is 20, llvmlite only officially
+# supports 22.
+# +++++++++++++++
 # Having two different versions of libLLVM in the same process is causing segfaults, this is 
-# achieved in practice by e.g. using Numba->llvmlite->libllvm20.so in the same process as 
+# achieved in practice by e.g. using Numba->llvmlite->libllvm22.so in the same process as
 # Pytorch->libLLVM19.so (or something else using a different version of LLVM).
-# llvmlite is the only package on main having a dependency on llvmdev 20. 
+# llvmlite is the only package on main having a dependency on llvmdev 22.
 # Statically linking against llvm prevents a point of collision.
 # see https://anaconda.atlassian.net/browse/PKG-7254
 export LLVMLITE_SHARED=0
